d6d520e4f192876d9b89a12e9c20745ac43cbf80
Philhawk <student@Dans-MacBook-Pro-2.local>
2016-11-11 14:41:45 -0500

    re-added without sensitive data

---
+++ index.js
@@ -0,0 +1,213 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
+
+// Facebook Webhook
+app.get('/webhooks', function (req, res) {
+  console.log(req.query)
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
+
+app.post('/webhooks', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else if (event.postback) {
+          receivedPostback(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    if (payload === 'Lets get in touch') {
+      sendTextMessage(senderID, "Thanks for getting in touch! Now we all know that getting to know someone is hard, so why dont we make this process a little easier?");
+      sendTextMessage(senderID, "What would you like to know?");
+      sendIntroMessage(senderID, "Potato called");
+    } else {
+      // When a postback is called, we'll send a message back to the sender to
+      // let them know it was successful
+      sendTextMessage(senderID, "Postback called");
+    }
+}
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+    contextMatcher(senderID, messageText)
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Um, sure ... thanks for that");
+  }
+}
+
+function contextMatcher(senderID, messageText){
+  switch(true){
+    case /generic/.test(messageText):
+      sendGenericMessage(senderID);
+      break;
+    case /experience/.test(messageText):
+      sendGenericMessage(senderID);
+      break;
+    default:
+      sendTextMessage(senderID, messageText);
+  }
+}
+
+function sendIntroMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    quick_replies: [{
+      content_type: "text",
+      title: "What's your story?",
+      payload: "personallife"
+    }, {
+      content_type: "text",
+      title: "Where are you from?",
+      payload: "wherefrom"
+    }]
+  }
+}
+
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
